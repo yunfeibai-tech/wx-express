5c2786d820b13c436dfa7d017fbc06f6afba413a
baiyf <b15031560723@163.com>
2018-07-24 10:44:19 +0800

    登录接口

--- routes/index.js
+++ routes/index.js
@@ -58,15 +58,37 @@ router.get('/userList',function (req,resUser,next) {
         var token = res;
         return new Promise(function(resolve, reject){
             request('https://api.weixin.qq.com/cgi-bin/user/get?access_token='+token, function(err, respond, data){
-                JSON.parse(data).data.openid.forEach(function (obj,index) {
+                var openid =[];
+                if(JSON.parse(data).data){
+                     openid =  JSON.parse(data).data.openid;
+                }
+                openid.forEach(function (obj,index) {
                     getUserInfo(obj).then(function (userInfo) {
-                        console.log(userInfo)
                         dataList.userList.push(userInfo)
                     })
-                })
-                setTimeout(function () {
-                    resUser.status(200).json(dataList)
-                },200)
+                });
+                //todo
+                var params ={
+                    "subscribe": 1,
+                    "openid": "o6_bmjrPTlm6_2sgVt7hMZOPfL2M",
+                    "nickname": "Band",
+                    "sex": 1,
+                    "language": "zh_CN",
+                    "city": "广州",
+                    "province": "广东",
+                    "country": "中国",
+                    "headimgurl":"http://thirdwx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/0",
+                    "subscribe_time": 153269495700,
+                    "unionid": " o6_bmasdasdsad6_2sgVt7hMZOPfL",
+                    "groupid": 0,
+                    "tagid_list":[128,2],
+                    "subscribe_scene": "ADD_SCENE_QR_CODE",
+                    "qr_scene": 98765,
+                    "qr_scene_str": ""
+                };
+                dataList.userList.push(params);
+                resUser.status(200).json(dataList)
+
 
             });
         });
@@ -75,9 +97,25 @@ router.get('/userList',function (req,resUser,next) {
     });
 })
 //token 接口
-router.get('/getToken',function (req,res,next) {
-    getToken().then(function (respond) {
-        res.status(200).json({assese_token:respond});
-    })
+router.post('/login',function (req,res,next) {
+    var params ={
+        "subscribe": 1,
+        "openid": "o6_bmjrPTlm6_2sgVt7hMZOPfL2M",
+        "nickname": "Band",
+        "sex": 1,
+        "language": "zh_CN",
+        "city": "广州",
+        "province": "广东",
+        "country": "中国",
+        "headimgurl":"http://thirdwx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/0",
+        "subscribe_time": 153269495700,
+        "unionid": " o6_bmasdasdsad6_2sgVt7hMZOPfL",
+        "groupid": 0,
+        "tagid_list":[128,2],
+        "subscribe_scene": "ADD_SCENE_QR_CODE",
+        "qr_scene": 98765,
+        "qr_scene_str": ""
+    };
+    res.status(200).json(params);
 })
 module.exports = router;
